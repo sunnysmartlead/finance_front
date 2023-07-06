228aad80fc2a736a9f40947e0c6f50e6bc5be90a
ljc <747301367@qq.com>
2023-07-06 12:24:38 +0800

    fix: bugfix

--- src/views/basicLibrary/service/publicMaterial.ts
+++ src/views/basicLibrary/service/publicMaterial.ts
@@ -11,7 +11,7 @@ export function getQueryPublicMaterialWarehouse(data: QueryPublicMaterialWarehou
 }
 
 /** 查询共用物料库 */
-export function deleteMultiplePublicMaterials(data: { ids: string[] }): any {
+export function deleteMultiplePublicMaterials(data: any): any {
   return request({
     url: "/api/services/app/UnitPriceLibrary/DeleteMultiplePublicMaterials",
     method: "delete",

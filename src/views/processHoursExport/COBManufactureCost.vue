a822b8b8b8618761294d104a8ae51afdedc373dd
王丹 <18174403058@163.com>
2023-08-24 19:12:29 +0800

    财务部录入保存和提交

--- src/views/processHoursExport/COBManufactureCost.vue
+++ src/views/processHoursExport/COBManufactureCost.vue
@@ -12,7 +12,10 @@
                 </div> -->
             </div>
             <div>
-                <el-button type="primary">提交</el-button>
+                <el-button type="primary" :disabled="tableData.length < 1 || tableData[0].logisticscostList?.length < 1"
+                        @click="submitData()">提交</el-button>
+                    <el-button type="primary" :disabled="tableData.length < 1 || tableData[0].logisticscostList?.length < 1"
+                        @click="saveTableData()">保存</el-button>
             </div>
         </div>
 
@@ -25,7 +28,7 @@
             </div>
         </div>
         <!-- COB-UPH表格区 -->
-        <el-card class="u-m-b-30">
+        <el-card class="u-m-b-30" v-if="COBUPHData.isCOB">
             <el-scrollbar wrap-style="padding:10px 0px;">
                 <div class="u-flex u-row-left u-col-center u-text-center">
                     <div style="background-color: rgb(247, 247, 96);">
@@ -36,14 +39,14 @@
                             COB-UPH
                         </div>
                     </div>
-                    <template v-if="COBUPHData.length>0">
-                        <div v-for="(uph, uphIndex) in COBUPHData" :key="uphIndex"
+                    <template v-if="COBUPHData.processHoursEnterUphBomItemsList.length>0">
+                        <div v-for="(uph, uphIndex) in COBUPHData.processHoursEnterUphBomItemsList" :key="uphIndex"
                             style="background-color: rgb(247, 247, 96);;">
                             <div class="u-border u-width-200  u-height-26">
                                 <span>{{ uph.year }}</span>
                             </div>
                             <div class="u-border u-width-200  u-height-26">
-                                <span>{{ uph.UPHValue }}</span>
+                                <span>{{ uph.comPuh }}</span>
                             </div>
                         </div>
                     </template>
@@ -78,35 +81,35 @@
                                             <el-table-column label="直接人工" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.directLaborPrice }}</span> -->
-                                                    <el-input-number v-model="scope.row.directLaborPrice" 
+                                                    <el-input-number v-model="scope.row.directLaborPrice" @change="directChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="设备折旧" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.directDepreciation }}</span> -->
-                                                    <el-input-number v-model="scope.row.directDepreciation" 
+                                                    <el-input-number v-model="scope.row.directDepreciation"  @change="directChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="换线成本" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.directLineChangeCost }}</span> -->
-                                                    <el-input-number v-model="scope.row.directLineChangeCost" 
+                                                    <el-input-number v-model="scope.row.directLineChangeCost"  @change="directChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="制造费用" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.directManufacturingCosts }}</span> -->
-                                                    <el-input-number v-model="scope.row.directManufacturingCosts" 
+                                                    <el-input-number v-model="scope.row.directManufacturingCosts" @change="directChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="小计" width="200" align="center"
                                                 label-class-name="labelClassName">
                                                 <template #default="scope">
-                                                    <span>{{ scope.row.directSummary }}</span>
+                                                    <span>{{ scope.row.directSummary?Number(scope.row.directSummary).toFixed(2):0.00 }}</span>
                                                 </template>
                                             </el-table-column>
                                         </el-table-column>
@@ -114,14 +117,14 @@
                                             <el-table-column label="直接人工" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.indirectLaborPrice }}</span> -->
-                                                    <el-input-number v-model="scope.row.indirectLaborPrice" 
+                                                    <el-input-number v-model="scope.row.indirectLaborPrice"  @change="indirectChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="设备折旧" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.indirectDepreciation }}</span> -->
-                                                    <el-input-number v-model="scope.row.indirectDepreciation" 
+                                                    <el-input-number v-model="scope.row.indirectDepreciation"  @change="indirectChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
@@ -129,20 +132,20 @@
                                             <el-table-column label="制造费用" width="200" align="center">
                                                 <template #default="scope">
                                                     <!-- <span>{{ scope.row.indirectManufacturingCosts }}</span> -->
-                                                    <el-input-number v-model="scope.row.indirectManufacturingCosts" 
+                                                    <el-input-number v-model="scope.row.indirectManufacturingCosts"  @change="indirectChange($event, scope.$index, scope.row,dataIndex)"
                                                                 :precision="2" :step="0.01"/>
                                                 </template>
                                             </el-table-column>
                                             <el-table-column label="小计" width="200" align="center"
                                                 label-class-name="labelClassName">
                                                 <template #default="scope">
-                                                    <span>{{ scope.row.indirectSummary }}</span>
+                                                    <span>{{ scope.row.indirectSummary?Number(scope.row.indirectSummary).toFixed(2):0.00}}</span>
                                                 </template>
                                             </el-table-column>
                                         </el-table-column>
                                         <el-table-column label="合计成本" align="center" width="200">
                                             <template #default="scope">
-                                                <span>{{ scope.row.totalCost }}</span>
+                                                <span>{{ scope.row.totalCost?Number(scope.row.totalCost).toFixed(2):0.00 }}</span>
                                             </template>
                                         </el-table-column>
                                         <el-table-column label="备注" align="center" width="200">
@@ -193,7 +196,7 @@
 import { ref, onMounted } from 'vue';
 import getQuery from "@/utils/getQuery";
 import { ElMessage, ElMessageBox, genFileId } from "element-plus"
-import { GetListAll, update, create, deleteItem, uploadAction } from "@/api/COB";
+import { GetListAll,getCOBUPH, update, create,handleSubmit, deleteItem, uploadAction } from "@/api/COB";
 //方案下拉选项
 const planOptions = ref([
     {
@@ -222,17 +225,11 @@ const queryParam = ref({
 })
 const { auditFlowId, productId }: any = getQuery()
 //COB-UPH
-const COBUPHData = ref<any>([])
-let tempUPHData = [
-    {
-        year: '15/Jul',
-        UPHValue: 'xxx',
-    },
-    {
-        year: '16/Jul',
-        UPHValue: 'xxx',
-    }
-]
+const COBUPHData = ref<any>({
+    processHoursEnterUphBomItemsList:[],
+    isCOB:false,
+})
+
 //表格数据
 const tableData = ref<any>([])
 let tempTableDataArr = [
@@ -293,29 +290,113 @@ onMounted(async () => {
     initData();
 })
 
-const getListData = async () => {
-    let param = queryParam.value;
-    if (param.SolutionId != undefined && param.AuditFlowId != undefined) {
-        await GetListAll(param).then((response: any) => {
-            if (response.success) {
-                let data = response.result;
-                tableData.value = data;
-                console.log("COB列表数据", data);
-            } else {
-                ElMessage({
-                    type: 'error',
-                    message: 'COB列表数据加载失败'
-                })
-            }
-        })
-    }
+const getListData = async (param:any) => {
+    await GetListAll(param).then((response: any) => {
+        if (response.success) {
+            let data = response.result;
+            tableData.value = data;
+            console.log("COB列表数据", data);
+        } else {
+            ElMessage({
+                type: 'error',
+                message: 'COB列表数据加载失败'
+            })
+        }
+    })
+}
+
+const getCOBUPHData=async (param:any)=>{
+    await getCOBUPH(param).then((response: any) => {
+        if (response.success) {
+            let data = response.result;
+            COBUPHData.value = data;
+            console.log("======COBUPHData=====", data);
+        } else {
+            ElMessage({
+                type: 'error',
+                message: 'COB列表数据加载失败'
+            })
+        }
+    })
 }
 
 const initData = () => {
-    COBUPHData.value = tempUPHData;
-    //tableData.value = tempTableDataArr;
-    getListData();
+    let param = queryParam.value;
+    if (param.SolutionId != undefined && param.AuditFlowId != undefined) {
+        getCOBUPHData(param);
+        getListData(param);
+    }
 }
+
+const directChange=(value: any, index: any, item: any,tableIndex:number) => {
+    let directLaborPrice= item.directLaborPrice?item.directLaborPrice:0.00;
+    let directDepreciation= item.directDepreciation?item.directDepreciation:0.00;
+    let directManufacturingCosts= item.directManufacturingCosts?item.directManufacturingCosts:0.00;
+    let directLineChangeCost= item.directLineChangeCost?item.directLineChangeCost:0.00;
+    let subTotal=Number(directLaborPrice)+Number(directDepreciation)+Number(directManufacturingCosts)+Number(directLineChangeCost)
+    tableData.value[tableIndex].listBomEnter[index].directSummary=subTotal;
+    let indirectSummary=tableData.value[tableIndex].listBomEnter[index].indirectSummary
+    tableData.value[tableIndex].listBomEnter[index].totalCost=subTotal+(indirectSummary?indirectSummary:0.00);
+}    
+
+const indirectChange=(value: any, index: any, item: any,tableIndex:number) => {
+    let indirectLaborPrice= item.indirectLaborPrice?item.indirectLaborPrice:0.00;
+    let indirectDepreciation= item.indirectDepreciation?item.indirectDepreciation:0.00;
+    let indirectManufacturingCosts= item.indirectManufacturingCosts?item.indirectManufacturingCosts:0.00;
+    let subTotal=Number(indirectLaborPrice)+Number(indirectDepreciation)+Number(indirectManufacturingCosts);
+    tableData.value[tableIndex].listBomEnter[index].indirectSummary=subTotal;
+    let directSummary=tableData.value[tableIndex].listBomEnter[index].directSummary
+    tableData.value[tableIndex].listBomEnter[index].totalCost=subTotal+(directSummary?directSummary:0.00);
+}
+
+//保存
+const saveTableData = () => {
+    let param = {
+        auditFlowId: queryParam.value.AuditFlowId,
+        solutionId: queryParam.value.SolutionId,
+        listBomEnter: JSON.parse(JSON.stringify(tableData.value))
+    };
+    console.log("财务部提交保存参数",param);
+    create(param).then((response: any) => {
+        console.log("保存响应", response);
+        if (response.success) {
+            ElMessage({
+                type: 'success',
+                message: '保存成功'
+            })
+           initData();
+        } else {
+            ElMessage({
+                type: 'error',
+                message: '保存失败'
+            })
+        }
+    })
+}
+//提交
+const submitData = () => {
+    let param = {
+        auditFlowId: queryParam.value.AuditFlowId,
+        solutionId: queryParam.value.SolutionId,
+    };
+    handleSubmit(param).then((response: any) => {
+        console.log("提交响应", response);
+        if (response.success) {
+            ElMessage({
+                type: 'success',
+                message: response.result
+            })
+            initData();
+        } else {
+            ElMessage({
+                type: 'error',
+                message: '失败'
+            })
+        }
+    })
+}
+
+
 </script>
 <style>
 .table-box {

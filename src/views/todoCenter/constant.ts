f17894861258bccc64bc2a3624a2fdccd6e520fe
NBSeven <410805964@qq.com>
2023-09-20 15:38:32 +0800

    fix: urlmap和国家库

--- src/views/todoCenter/constant.ts
+++ src/views/todoCenter/constant.ts
@@ -37,36 +37,42 @@
 // }
 // export default urlMap
 const urlMap = {
-  PricingDemandInput:{ path:"/demandApply/index",query: {} },//核价需求
-  PriceDemandReview:{path:"/demandApplyAudit/index",query: {} },//核价需求审核&方案录入
-  TRToExamine:{path:"/pddAudit/index",query: {} },//产品开发部TR审核
-  StructureBOM:{path:"/structuralMaterialImport/index",query: {} },//结构bom导入
-  StructureBOMToExamine:{path:"/bomView/struc",query: {} },//结构bom导入审核
-  ElectronicsBOM:{path:"/electronicImport/index",query: {} },//电子Bom导入
-  ElectronicsBOMToExamine:{path:"/bomView/elec",query: {} },//电子Bom导入审核
-  LogisticsCostEntry:{path:"/processHoursExport/logisticsCost",query: {}},//物流成本录入
-  FormulaOperationAddition:{path:"/processHoursExport/index",query: {}},//工序工时导入
-  CustomizedStructuralComponents:{},//定制结构件
-  TradeCompliance:{path:"/tradeCompliance/index",query: {}},//贸易合规
-  COBManufacturingCostEntry:{path:"/processHoursExport/COBManufactureCost",query: {}},//COM制造成本录入
-  ElectronicUnitPriceEntry:{path:"/resourcesDepartment/electronic",query: {}},//电子单价录入
-  ElectronicUnitPriceEntryToExamine:{path:"/bomVerify/electronic",query: {}},//电子单价录入审核
-  StructureUnitPriceEntry:{path:"/resourcesDepartment/construction",query: {}},//结构单价录入
-  StructureUnitPriceEntryToExamine:{path:"/bomVerify/construction",query:{}},//结构单价录入审核
-  UnitPriceInputReviewToExamine:{path:"/bomVerify/index",query:{}},//BOM单价录入审核
+  PricingDemandInput: { path: "/demandApply/index", query: {} }, //核价需求
+  PriceDemandReview: { path: "/demandApplyAudit/index", query: {} }, //核价需求审核&方案录入
+  TRToExamine: { path: "/pddAudit/index", query: {} }, //产品开发部TR审核
+  StructureBOM: { path: "/structuralMaterialImport/index", query: {} }, //结构bom导入
+  StructureBOMToExamine: { path: "/bomView/struc", query: {} }, //结构bom导入审核
+  ElectronicsBOM: { path: "/electronicImport/index", query: {} }, //电子Bom导入
+  ElectronicsBOMToExamine: { path: "/bomView/elec", query: {} }, //电子Bom导入审核
+  LogisticsCostEntry: { path: "/processHoursExport/logisticsCost", query: {} }, //物流成本录入
+  FormulaOperationAddition: { path: "/processHoursExport/index", query: {} }, //工序工时导入
+  CustomizedStructuralComponents: {}, //定制结构件
+  TradeCompliance: { path: "/tradeCompliance/index", query: {} }, //贸易合规
+  COBManufacturingCostEntry: { path: "/processHoursExport/COBManufactureCost", query: {} }, //COM制造成本录入
+  ElectronicUnitPriceEntry: { path: "/resourcesDepartment/electronic", query: {} }, //电子单价录入
+  ElectronicUnitPriceEntryToExamine: { path: "/bomVerify/electronic", query: {} }, //电子单价录入审核
+  StructureUnitPriceEntry: { path: "/resourcesDepartment/construction", query: {} }, //结构单价录入
+  StructureUnitPriceEntryToExamine: { path: "/bomVerify/construction", query: {} }, //结构单价录入审核
+  UnitPriceInputReviewToExamine: { path: "/bomVerify/index", query: {} }, //BOM单价录入审核
 
-  NRE_EMCExperimentalFeeInput:{path:"/nre/nrePilotprojects",query: {}},//EMC实验费录入
-  NRE_EMCExperimentalFeeInputToExamine:{path:"/nre/nrePilotprojectsVertify",query: {}},//EMC实验费录入审核
-  NRE_ReliabilityExperimentFeeInput:{path:"/nre/nreExperimentItems",query: {}},//可靠性实验费录入
-  NRE_ReliabilityExperimentFeeInputoExamine:{path:"/nre/nreExperimentItemsVertify",query: {}},//可靠性实验费录入审核
-  NRE_MoldFeeEntry:{path:"/nre/nreResourcesDepartment",query: {}},//模具费录入
-  NRE_MoldFeeEntryToExamine:{path:"/nre/nreResourcesDepartmentVertify",query: {}},//模具费审核
-  NRE_ManualComponentInput:{path:"/nre/nreProjectManageMent",query: {}},//手板件费录入
-  PriceEvaluationBoard:{path:"/dashboard/index",query: {}},//核价看板
-  FinanceDirectorAudit:{path:"/financeDirectorAudit",query: {}},// 财务部部长审核
-  ProjectChiefAudit:{path:"/projectChiefAudit",query: {}},// 项目科长审核
-  ProjectDirectorLook:{path:"/projectDirectorLook",query: {}},// 项目部长查看
+  NRE_EMCExperimentalFeeInput: { path: "/nre/nrePilotprojects", query: {} }, //EMC实验费录入
+  NRE_EMCExperimentalFeeInputToExamine: { path: "/nre/nrePilotprojectsVertify", query: {} }, //EMC实验费录入审核
+  NRE_ReliabilityExperimentFeeInput: { path: "/nre/nreExperimentItems", query: {} }, //可靠性实验费录入
+  NRE_ReliabilityExperimentFeeInputoExamine: { path: "/nre/nreExperimentItemsVertify", query: {} }, //可靠性实验费录入审核
+  NRE_MoldFeeEntry: { path: "/nre/nreResourcesDepartment", query: {} }, //模具费录入
+  NRE_MoldFeeEntryToExamine: { path: "/nre/nreResourcesDepartmentVertify", query: {} }, //模具费审核
+  NRE_ManualComponentInput: { path: "/nre/nreProjectManageMent", query: {} }, //手板件费录入
+  PriceEvaluationBoard: { path: "/dashboard/index", query: {} }, //核价看板
+  FinanceDirectorAudit: { path: "/financeDirectorAudit", query: {} }, // 财务部部长审核
+  ProjectChiefAudit: { path: "/projectChiefAudit", query: {} }, // 项目科长审核
+  ProjectDirectorLook: { path: "/projectDirectorLook", query: {} }, // 项目部长查看
   // 报价分析看板
-  QuoteAnalysis:{path:"/quoteAnalysis/index",query: {}}, // 报价分析看板
+  QuoteAnalysis: { path: "/quoteAnalysis/index", query: {} }, // 报价分析看板
+  ConfirmWinningBid: { path: "/marketingQuotation/indexSecond", query: {} }, //确认中标
+  CoreNre: { path: "/quoteAnalysis/nreCoreDevices", query: {} }, //核心部件
+  QuoteFeedback: { path: "/quoteAnalysis/quoteForm", query: {} }, //（报价反馈）
+  BidWinningConfirmation: { path: "/marketingQuotation/indexSecond", query: {} }, // （财务中标确认）
+  ArchiveEnd: { path: "/archive/index", query: {} }, //"归档结束（异常终止）"
+  QuoteApproval: { path: "/marketingQuotation/indexFirst", query: {} } //"总经理报价审批界面"
 }
 export default urlMap

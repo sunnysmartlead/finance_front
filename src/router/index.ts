b4e25d098c550b8bd39bec2fe1e49ba0e878cb1e
littER <38061095+superheroman@users.noreply.github.com>
2023-09-25 17:58:32 +0800

    fix: bugfix

--- src/router/index.ts
+++ src/router/index.ts
@@ -450,7 +450,7 @@ export const constantRoutes: Array<RouteRecordRaw> = [
       {
         path: "/nre/nrePilotprojects",
         component: () => import("@/views/nre/nrePilotprojects.vue"),
-        name: "NrePilotprojects",
+        name: "nrePilotprojects",
         meta: {
           title: "产品部EMC实验费录入"
         }
@@ -458,7 +458,7 @@ export const constantRoutes: Array<RouteRecordRaw> = [
       {
         path: "/nre/nrePilotprojectsVertify",
         component: () => import("@/views/nre/vertify/nrePilotprojects.vue"),
-        name: "NrePilotprojectsVertify",
+        name: "nrePilotprojectsVertify",
         meta: {
           title: "产品部EMC实验费录入审核"
         }

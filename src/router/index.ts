cfcc27e57f0f5199f96fac43dba4d0194bec1264
littER <38061095+superheroman@users.noreply.github.com>
2023-11-28 23:43:41 +0800

    fix: 解决打包问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -61,14 +61,6 @@ export const constantRoutes: Array<RouteRecordRaw> = [
         }
       },
       {
-        path: "/demandApply/fast",
-        component: () => import("@/views/demandApply/fast.vue"),
-        name: "fastDemandApply",
-        meta: {
-          title: "快速核价需求录入"
-        }
-      },
-      {
         path: "/demandApplyAudit/index",
         component: () => import("@/views/demandApplyAudit/index.vue"),
         name: "demandApplyAudit",
